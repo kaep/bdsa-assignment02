3ada93420c639dda9c1aff07e9db396fd27ff321
kaeppen <seho@itu.dk>
2021-09-17 13:07:47 +0200

    Implement Student class and first 3 properties

---
+++ assignment02/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace assignment02
+{
+    public class Student
+    {
+        public int Id 
+        {
+            get { return Id; }
+            set { Id = value; }
+        } 
+
+        public string GivenName
+        {
+            get {return GivenName; }
+            set {GivenName = value; }
+        } 
+
+        public string Surname 
+        {
+            get {return Surname; }
+            set {Surname = value; }
+        }
+
+        public Status Status 
+    }
+}
